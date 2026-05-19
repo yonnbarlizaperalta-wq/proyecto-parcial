352333a1253471012ff6b17d89c07547e590b3c8
yonnbarlizaperalta-wq <yonnbarlizaperalta@gmail.com>
2026-05-19 11:02:52 -0500

    agregamos metodo para obtener el pais por medio del nombre

--- country_api.py
+++ country_api.py
@@ -1,7 +1,15 @@
 import requests
 from country import Country
-
-
 class CountryAPI:
     def __init__(self):
         self.base_url = "https://restcountries.com/v3.1"
+           
+    def by_name(self, name: str):
+            url = f"{self.base_url}/name/{name}"
+            response = requests.get(url)
+            if response.status_code != 200:
+                 return None
+            data = response.json()
+            if isinstance(data, list) and len(data) > 0:
+                 return Country(data[0])
+            return None
