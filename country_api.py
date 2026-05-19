e1bbba48d3f332ee1610477fa91deadcc2d04c58
yonnbarlizaperalta-wq <yonnbarlizaperalta@gmail.com>
2026-05-19 13:04:22 -0500

    preparamos para solicitudes simultaneas

--- country_api.py
+++ country_api.py
@@ -1,10 +1,10 @@
 import requests
 from country import Country
+from concurrent.futures import ThreadPoolExecutor
 class CountryAPI:
     def __init__(self):
         self.base_url = "https://restcountries.com/v3.1"
-           
-    def by_name(self, name: str):
+        def by_name(self, name: str):
             url = f"{self.base_url}/name/{name}"
             response = requests.get(url)
             if response.status_code != 200:
@@ -25,4 +25,3 @@ class CountryAPI:
             countries.append(Country(item))
             return countries
          
-         holaaaaaaaaaaaaaaaaaaaaaaaaa
