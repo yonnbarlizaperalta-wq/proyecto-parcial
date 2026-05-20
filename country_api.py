91460ca3c08dfe004b06c3b2fe5c108d9f24eb56
yonnbarlizaperalta-wq <yonnbarlizaperalta@gmail.com>
2026-05-19 20:50:09 -0500

    correccion

--- country_api.py
+++ country_api.py
@@ -1,35 +1,35 @@
 import requests
 from country import Country
-from concurrent.futures import ThreadPoolExecutor
+
+
 class CountryAPI:
     def __init__(self):
         self.base_url = "https://restcountries.com/v3.1"
-        def by_name(self, name: str):
-            url = f"{self.base_url}/name/{name}"
-            response = requests.get(url)
-            if response.status_code != 200:
-                 return None
-            data = response.json()
-            if isinstance(data, list) and len(data) > 0:
-                 return Country(data[0])
+
+    def by_name(self, name: str):
+        url = f"{self.base_url}/name/{name}"
+
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            print("Error con:", name)   # 👈 IMPORTANTE PARA DEBUG
             return None
-    
-    def by_region(self, region: str):
-         url = f"{self.base_url}/region/{region}"
-         response = requests.get(url)
-         if response.status_code != 200:
-              return []
-         data = response.json()
-         countries = []
-         for item in data:
-            countries.append(Country(item))
-            return countries
-         def by_names(self, names: list):
-             countries = []
-             with ThreadPoolExecutor() as executor:
-                 results = executor.map(self.by_name, names) 
-                 for country in results:
-                     if country:
-                         countries.append(country)
-                         return countries
+
+        data = response.json()
+
+        # 👇 ESTO ES LA CLAVE
+        if isinstance(data, list) and len(data) > 0:
+            return Country(data[0])
+
+        return None
+
+    def by_names(self, names: list):
+        countries = []
+
+        for name in names:
+            c = self.by_name(name)
+            if c is not None:
+                countries.append(c)
+
+        return countries
                      
